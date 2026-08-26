e1132f00eb7056b59daaa625c441275302268407
andreananda1022 <andreananda1022@gmail.com>
2026-08-26 08:46:59 +0700

    Add Request Timeout Duration

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use tokio::net::TcpListener;
 use std::error::Error;
+use std::time::Duration;
 use std::sync::{Arc, Mutex};
+
+const MAX_CONNECTIONS: usize = 100;
+const TIMEOUT_DURATION: Duration = Duration::from_secs(10);
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -8,7 +12,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     println!("Server berjalan di http://127.0.0.1:8080");
 
     let counter = Arc::new(Mutex::new(0));
-    tokio_http_server::run(listener, counter).await?;
+    tokio_http_server::run(listener, counter, MAX_CONNECTIONS, TIMEOUT_DURATION).await?;
 
     Ok(())
 }
