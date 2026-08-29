35844b3d9efe9a7d33ada510e88e32a8a009ef93
andreananda1022 <andreananda1022@gmail.com>
2026-08-29 20:15:01 +0700

    refactor: encapsulate config parameters into ServerConfig

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::sync::{Arc, Mutex};
 
 const MAX_CONNECTIONS: usize = 100;
 const TIMEOUT_DURATION: Duration = Duration::from_secs(10);
+const MAX_LINE_SIZE: usize = 8192;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -12,7 +13,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
     println!("Server berjalan di http://127.0.0.1:8080");
 
     let counter = Arc::new(Mutex::new(0));
-    tokio_http_server::run(listener, counter, MAX_CONNECTIONS, TIMEOUT_DURATION).await?;
+    let config = tokio_http_server::ServerConfig {
+        max_connections: MAX_CONNECTIONS,
+        timeout_duration: TIMEOUT_DURATION,
+        max_line_size: MAX_LINE_SIZE
+    };
+    tokio_http_server::run(listener, counter, config).await?;
 
     Ok(())
 }
