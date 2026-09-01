c3da710c03ed1b39c3343c0aafe2192c21f2f36a
andreananda1022 <andreananda1022@gmail.com>
2026-09-01 08:54:28 +0700

    feat: add tracing crate for error-level logging

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use tokio::net::TcpListener;
 use std::error::Error;
-use std::time::Duration;
 use std::sync::{Arc, Mutex};
+use std::time::Duration;
+use tokio::net::TcpListener;
 
 const MAX_CONNECTIONS: usize = 100;
 const TIMEOUT_DURATION: Duration = Duration::from_secs(10);
@@ -9,14 +9,17 @@ const MAX_LINE_SIZE: usize = 8192;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let listener = TcpListener::bind("127.0.0.1:8080").await?;   
-    println!("Server berjalan di http://127.0.0.1:8080");
+    let filter = tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| "info".into());
+    tracing_subscriber::fmt().with_env_filter(filter).init();
+
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+    tracing::info!("Server berjalan di http://127.0.0.1:8080");
 
     let counter = Arc::new(Mutex::new(0));
     let config = tokio_http_server::ServerConfig {
         max_connections: MAX_CONNECTIONS,
         timeout_duration: TIMEOUT_DURATION,
-        max_line_size: MAX_LINE_SIZE
+        max_line_size: MAX_LINE_SIZE,
     };
     tokio_http_server::run(listener, counter, config).await?;
 
